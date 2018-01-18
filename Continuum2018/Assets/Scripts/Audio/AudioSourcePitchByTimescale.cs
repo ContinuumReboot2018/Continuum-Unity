2daa3437a694c6c54c34ee3f82afa37f6fcd4f5b
Andrew Spalato <7651775@student.swin.edu.au>
2018-01-18 20:36:34 +1100

    Upgraded to unity 2017.3.0f3

--- Continuum2018/Assets/Scripts/Audio/AudioSourcePitchByTimescale.cs
+++ Continuum2018/Assets/Scripts/Audio/AudioSourcePitchByTimescale.cs
@@ -1,13 +1,5 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.Audio;
-
-/* Instructions: 
-
-1. Attach script to gameObject with an audio source
-2. Set values
-
- */
 
 public class AudioSourcePitchByTimescale : MonoBehaviour 
 {
@@ -20,50 +12,20 @@ public class AudioSourcePitchByTimescale : MonoBehaviour
 	public bool dontUseStartPitch;
 
 	private AudioSource Audio;
-	
+
+	void Awake ()
+	{
+		Audio = GetComponent<AudioSource> ();
+	}
+
 	void Start () 
 	{
-		FindAudioSource ();
-
-		if (dontUseStartPitch == false) 
-		{
-			Audio.pitch = startingPitch; // Gives value to audio pitch
-		}
-
-		if (dontUseStartPitch == true) 
-		{
-			Audio.pitch = Time.timeScale; // Sets starting pitch based on Time.timeScale.
-		}
+		Audio.pitch = dontUseStartPitch ? startingPitch : Time.timeScale; // Gives value to audio pitch.
 	}
-	
 
 	void Update ()
 	{
 		Audio.pitch = (Time.timeScale * multiplierPitch * startingPitch) + addPitch;
-
-		if (Audio.pitch < 0) 
-		{
-			Debug.LogWarning ("Audio pitch is " + Audio.pitch + ".");
-		}
-
 		Audio.pitch = Mathf.Clamp (Audio.pitch, minimumPitch, maximumPitch);
-
-		// Gives maximum pitch to audio source before reaching it
-		if (Audio.pitch > maximumPitch)
-		{
-			if (reachedMaxPitch == false)
-			{
-				Audio.pitch = maximumPitch;
-				Debug.Log ("Reached maximum audio pitch.");
-				reachedMaxPitch = true;
-			}
-
-			Audio.pitch = maximumPitch;
-		}
-	}
-
-	void FindAudioSource ()
-	{
-		Audio = GetComponent<AudioSource> ();
 	}
 }
