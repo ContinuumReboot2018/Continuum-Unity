b450abc5698f6cc9f922ff41796e620f5b12ae50
Andrew Spalato <7651775@student.swin.edu.au>
2018-05-23 11:05:44 +1000

    Implemented mods into the game.

--- Continuum2018/Assets/Scripts/Managers/Game/GameModifierReceiver.cs
+++ Continuum2018/Assets/Scripts/Managers/Game/GameModifierReceiver.cs
@@ -1,63 +1,151 @@
 ﻿using UnityEngine;
+using TMPro;
 
 public class GameModifierReceiver : MonoBehaviour 
 {
 	public GameModifierManager gameModifierManagerScript;
+	[Space (10)]
+	public TextMeshProUGUI TutorialStatusText;
+	public TextMeshProUGUI PowerupModeStatusText;
+	public TextMeshProUGUI BossModeStatusText;
+	public TextMeshProUGUI TimeIncreaseModeStatusText;
+	public TextMeshProUGUI AlwaysHomingStatusText;
+	public TextMeshProUGUI AlwaysRicochetStatusText;
+	public TextMeshProUGUI AlwaysRapidfireStatusText;
+	public TextMeshProUGUI AlwaysOverdriveStatusText;
+	public TextMeshProUGUI StartingLivesStatusText;
+	public TextMeshProUGUI BlockSpawnRateStatusText;
+	public TextMeshProUGUI StackingStatusText;
+	public TextMeshProUGUI OverheatStatusText;
+
+	void Start ()
+	{
+		CheckModifierStates ();
+	}
 
 	// The below methods will be accessed by UI in the main menu.
 
-	public void SetTutorialState (bool tutorial)
+	public void SetTutorialState ()
 	{
-		gameModifierManagerScript.Tutorial = tutorial;
-		Debug.Log ("MODIFIER: Tutorial: " + tutorial.ToString ());
+		gameModifierManagerScript.Tutorial = !gameModifierManagerScript.Tutorial;
+		Debug.Log ("MODIFIER: Tutorial: " + gameModifierManagerScript.Tutorial.ToString ());
 	}
 
-	public void SetPowerupMode (int powerupMode)
+	public void SetPowerupMode ()
 	{
-		gameModifierManagerScript.PowerupSpawn = (GameModifierManager.powerupSpawnMode)powerupMode;
+		if (gameModifierManagerScript.PowerupSpawn < (GameModifierManager.powerupSpawnMode)3) 
+		{
+			gameModifierManagerScript.PowerupSpawn += 1;
+		} 
+
+		else 
+		
+		{
+			gameModifierManagerScript.PowerupSpawn = 0;
+		}
+
 		Debug.Log ("MODIFIER: Powerup spawning mode: " + gameModifierManagerScript.PowerupSpawn.ToString ());
 	}
 
-	public void SetBossSpawnMode (int bossMode)
+	public void SetBossSpawnMode ()
 	{
-		gameModifierManagerScript.BossSpawn = (GameModifierManager.bossSpawnMode)bossMode;
+		if (gameModifierManagerScript.BossSpawn < (GameModifierManager.bossSpawnMode)2) 
+		{
+			gameModifierManagerScript.BossSpawn += 1;
+		} 
+
+		else 
+		
+		{
+			gameModifierManagerScript.BossSpawn = 0;
+		}
+
 		Debug.Log ("MODIFIER: Boss spawning mode: " + gameModifierManagerScript.BossSpawn.ToString ());
 	}
 
-	public void SetTimeIncreaseMode (int timeIncreaseId)
+	public void SetTimeIncreaseMode ()
 	{
-		gameModifierManagerScript.TimeIncreaseMode = (GameModifierManager.timeIncreaseMode)timeIncreaseId;
+		if (gameModifierManagerScript.TimeIncreaseMode < (GameModifierManager.timeIncreaseMode)3) 
+		{
+			gameModifierManagerScript.TimeIncreaseMode += 1;
+		} 
+
+		else 
+
+		{
+			gameModifierManagerScript.TimeIncreaseMode = 0;
+		}
+			
 		Debug.Log ("MODIFIER: Time increase mode: " + gameModifierManagerScript.TimeIncreaseMode.ToString ());
 	}
 
-	public void SetStartingLives (int startingLives)
+	public void SetAlwaysHoming ()
 	{
-		gameModifierManagerScript.StartingLives = startingLives;
-		Debug.Log ("MODIFIER: Starting lives: " + startingLives.ToString ());
+		gameModifierManagerScript.AlwaysHoming = !gameModifierManagerScript.AlwaysHoming;
+		Debug.Log ("MODIFIER: Always homing: " + gameModifierManagerScript.AlwaysHoming.ToString ());
 	}
 
-	public void SetAlwaysHoming (bool alwaysHoming)
+	public void SetAlwaysRicochet ()
 	{
-		gameModifierManagerScript.AlwaysHoming = alwaysHoming;
-		Debug.Log ("MODIFIER: Always homing: " + alwaysHoming.ToString ());
+		gameModifierManagerScript.AlwaysRicochet = !gameModifierManagerScript.AlwaysRicochet;
+		Debug.Log ("MODIFIER: Always ricochet: " + gameModifierManagerScript.AlwaysRicochet.ToString ());
 	}
 
-	public void SetAlwaysRicochet (bool alwaysRicochet)
+	public void SetAlwaysRapidFire ()
 	{
-		gameModifierManagerScript.AlwaysRicochet = alwaysRicochet;
-		Debug.Log ("MODIFIER: Always ricochet: " + alwaysRicochet.ToString ());
+		gameModifierManagerScript.AlwaysRapidfire = !gameModifierManagerScript.AlwaysRapidfire;
+		Debug.Log ("MODIFIER: Always rapidfire: " + gameModifierManagerScript.AlwaysRapidfire.ToString ());
 	}
 
-	public void SetAlwaysRapidFire (bool alwaysRapidFire)
+	public void SetAlwaysOverdrive ()
 	{
-		gameModifierManagerScript.AlwaysRapidfire = alwaysRapidFire;
-		Debug.Log ("MODIFIER: Always rapidfire: " + alwaysRapidFire.ToString ());
+		gameModifierManagerScript.AlwaysOverdrive = !gameModifierManagerScript.AlwaysOverdrive;
+		Debug.Log ("MODIFIER: Always overdrive: " + gameModifierManagerScript.AlwaysOverdrive.ToString ());
 	}
 
-	public void SetAlwaysOverdrive (bool alwaysOverdrive)
+	public void SetStartingLives ()
 	{
-		gameModifierManagerScript.AlwaysOverdrive = alwaysOverdrive;
-		Debug.Log ("MODIFIER: Always overdrive: " + alwaysOverdrive.ToString ());
+		if (gameModifierManagerScript.StartingLives < 10)
+		{
+			gameModifierManagerScript.StartingLives += 1;
+		} 
+
+		else 
+		
+		{
+			gameModifierManagerScript.StartingLives = 1;
+		}
+
+		Debug.Log ("MODIFIER: Starting lives: " + gameModifierManagerScript.StartingLives.ToString ());
+	}
+
+	public void SetBlockSpawnRate ()
+	{
+		if (gameModifierManagerScript.blockSpawnRateMultiplier < 10) 
+		{
+			gameModifierManagerScript.blockSpawnRateMultiplier += 1;
+		} 
+
+		else 
+		
+		{
+			gameModifierManagerScript.blockSpawnRateMultiplier = 1;
+		}
+
+		Debug.Log ("MODIFIER: Block spawn rate multiplier: " + gameModifierManagerScript.blockSpawnRateMultiplier.ToString ());
+	}
+
+
+	public void SetStackingMode ()
+	{
+		gameModifierManagerScript.stacking = !gameModifierManagerScript.stacking;
+		Debug.Log ("MODIFIER: Stacking: " + gameModifierManagerScript.stacking.ToString ());
+	}
+
+	public void SetOverheatMode ()
+	{
+		gameModifierManagerScript.useOverheat = !gameModifierManagerScript.useOverheat;
+		Debug.Log ("MODIFIER: Use overheat: " + gameModifierManagerScript.useOverheat.ToString ());
 	}
 
 	public void SetTrialTime (float trialTime)
@@ -65,14 +153,63 @@ public class GameModifierReceiver : MonoBehaviour
 		gameModifierManagerScript.TrialTime = trialTime;
 	}
 
-	public void SetBlockSpawnRate (int BlockSpawnMult)
-	{
-		gameModifierManagerScript.blockSpawnRateMultiplier = BlockSpawnMult;
-		Debug.Log ("MODIFIER: Block spawn multiplier: " + BlockSpawnMult.ToString ());
-	}
 
-	public void SetStackingMode (bool allowStacking)
+	public void CheckModifierStates ()
 	{
-		gameModifierManagerScript.stacking = allowStacking;
+		TutorialStatusText.text = gameModifierManagerScript.Tutorial ? "ON" : "OFF";
+
+		switch (gameModifierManagerScript.PowerupSpawn) 
+		{
+		case GameModifierManager.powerupSpawnMode.Normal:
+			PowerupModeStatusText.text = "NORMAL";
+			break;
+		case GameModifierManager.powerupSpawnMode.Faster:
+			PowerupModeStatusText.text = "FASTER";
+			break;
+		case GameModifierManager.powerupSpawnMode.Slower:
+			PowerupModeStatusText.text = "SLOWER";
+			break;
+		case GameModifierManager.powerupSpawnMode.Off:
+			PowerupModeStatusText.text = "OFF";
+			break;
+		}
+
+		switch (gameModifierManagerScript.BossSpawn) 
+		{
+		case GameModifierManager.bossSpawnMode.Normal:
+			BossModeStatusText.text = "NORMAL";
+			break;
+		case GameModifierManager.bossSpawnMode.BossesOnly:
+			BossModeStatusText.text = "BOSS RUSH";
+			break;
+		case GameModifierManager.bossSpawnMode.NoBosses:
+			BossModeStatusText.text = "NO BOSSES";
+			break;
+		}
+			
+		switch (gameModifierManagerScript.TimeIncreaseMode) 
+		{
+		case GameModifierManager.timeIncreaseMode.Normal:
+			TimeIncreaseModeStatusText.text = "NORMAL";
+			break;
+		case GameModifierManager.timeIncreaseMode.Slow:
+			TimeIncreaseModeStatusText.text = "SLOW";
+			break;
+		case GameModifierManager.timeIncreaseMode.Fast:
+			TimeIncreaseModeStatusText.text = "FAST";
+			break;
+		case GameModifierManager.timeIncreaseMode.Off:
+			TimeIncreaseModeStatusText.text = "OFF";
+			break;
+		}
+
+		AlwaysHomingStatusText.text = gameModifierManagerScript.AlwaysHoming ? "ON" : "OFF";
+		AlwaysRicochetStatusText.text = gameModifierManagerScript.AlwaysRicochet ? "ON" : "OFF";
+		AlwaysRapidfireStatusText.text = gameModifierManagerScript.AlwaysRapidfire ? "ON" : "OFF";
+		AlwaysOverdriveStatusText.text = gameModifierManagerScript.AlwaysOverdrive ? "ON" : "OFF";
+		StartingLivesStatusText.text = gameModifierManagerScript.StartingLives.ToString ();
+		BlockSpawnRateStatusText.text = gameModifierManagerScript.blockSpawnRateMultiplier.ToString ();
+		StackingStatusText.text = gameModifierManagerScript.stacking ? "ON" : "OFF";
+		OverheatStatusText.text = gameModifierManagerScript.useOverheat ? "ON" : "OFF";
 	}
 }
