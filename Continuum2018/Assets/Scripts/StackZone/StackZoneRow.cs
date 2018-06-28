7465c18bcd697fb94b864ed037265f828092fbd5
Andrew Spalato <7651775@student.swin.edu.au>
2018-06-28 12:17:24 +1000

    Added row stack checker if row is fully occupied.

---
+++ Continuum2018/Assets/Scripts/StackZone/StackZoneRow.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class StackZoneRow : MonoBehaviour 
+{
+	public StackZone[] Rows;
+	public bool allRowsAreStacked;
+
+	void Start ()
+	{
+		GetRows ();
+		CheckRowStacked ();
+		InvokeRepeating ("CheckRowStacked", 0, 0.5f);
+	}
+
+	void GetRows ()
+	{
+		Rows = GetComponentsInChildren<StackZone> ();
+	}
+
+	void CheckRowStacked ()
+	{
+		for (int i = 0; i < Rows.Length; i++)
+		{
+			// found a stack which is not occupied.
+			if (Rows [i].isOccupied == false) 
+			{
+				allRowsAreStacked = false;
+				return;
+			}
+
+			if (Rows [Rows.Length - 1].isOccupied == true) 
+			{
+				allRowsAreStacked = true;
+			}
+		}
+	}
+}
