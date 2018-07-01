86cb122bcc6ef9d8c89898a38fb688722756a6e1
Andrew Spalato <7651775@student.swin.edu.au>
2018-07-01 12:59:50 +1000

    Added disclaimer scene and simple timed scene loader scirpt.

---
+++ Continuum2018/Assets/Scripts/Managers/SceneManagement/SimpleTimedSceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SimpleTimedSceneLoader : MonoBehaviour 
+{
+	public string SceneToLoad;
+	public float Delay = 5;
+
+	void Start ()
+	{
+		StartCoroutine (TimedSceneLoad ());
+	}
+
+	IEnumerator TimedSceneLoad ()
+	{
+		yield return new WaitForSecondsRealtime (Delay);
+		SceneManager.LoadScene (SceneToLoad);
+	}
+}
