ffa87a5df5e399a72d307bda745a437924557d9d
Andrew Spalato <7651775@student.swin.edu.au>
2018-07-03 15:13:32 +1000

    Branching off to try to make multiple instances of a black hole.

---
+++ Continuum2018/Assets/Scripts/Camera/BlackHole.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class BlackHole
+{
+	public Transform BH;
+	public float radius;
+	[HideInInspector]
+	public Material material;
+
+	public BlackHole (Transform _BH, float _radius, Material _material)
+	{
+		BH = _BH;
+		radius = _radius;
+		material = _material;
+	}
+}
